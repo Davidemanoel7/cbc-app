05a253750ca4962a0ebdc8c37922329198550806
Davidemanoel7 <david.emanoel.706@ufrn.edu.br>
2024-12-01 19:05:05 -0300

    cache provider

---
+++ lib/providers/cache_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CacheProvider extends ChangeNotifier {
+  Map<String, dynamic>? _userInfoKey;
+
+  String? _acessToken;
+  String? _refreshToken;
+
+  Map<String, dynamic>? get userInfor => _userInfoKey;
+
+  String? get acessToken => _acessToken;
+
+  String? get refreshToken => _refreshToken;
+
+  Future<void> initCache () async {
+    final pref = SharedPreferences.getInstance().then(
+      (pref) {
+        _acessToken = pref.getString('acess_token');
+        // Pegar outros dados...
+      }
+    );
+    // notifyListeners();
+  }
+
+  
+}
