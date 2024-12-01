947707d64f73126097741bbb10846b1be3eda7e1
Davidemanoel7 <david.emanoel.706@ufrn.edu.br>
2024-12-01 19:35:38 -0300

    increase get, set and remove functions provider

--- lib/providers/cache_provider.dart
+++ lib/providers/cache_provider.dart
@@ -13,15 +13,70 @@ class CacheProvider extends ChangeNotifier {
 
   String? get refreshToken => _refreshToken;
 
-  Future<void> initCache () async {
-    SharedPreferences.getInstance().then(
+  final Future<SharedPreferences> _pref = SharedPreferences.getInstance();
+
+  Future<void> initCache() async {
+    await _pref.then(
       (pref) {
         _acessToken = pref.getString('acess_token');
         // Pegar outros dados...
       }
     );
-    // notifyListeners();
+    notifyListeners();
   }
 
-  
+  Future<void> setItemFromCache( String key, TypeKey typeKey, dynamic value ) async {
+    switch( typeKey ) {
+      case TypeKey.string:
+        await _pref.then(( p ) => p.setString( key, value ));
+        return;
+      case TypeKey.int:
+        await _pref.then((p) => p.setInt( key, value ));
+        return;
+      case TypeKey.bool:
+        await _pref.then((p) => p.setBool( key, value ));
+        return;
+      case TypeKey.double:
+        await _pref.then((p) => p.setDouble( key, value ));
+        return;
+      default:
+        return;
+    }
+  }
+
+  Future<void> gettemFromCache( String key, TypeKey typeKey ) async {
+    switch( typeKey ) {
+      case TypeKey.string:
+        await _pref.then(( p ) => p.getString( key ));
+        return;
+      case TypeKey.int:
+        await _pref.then((p) => p.getInt( key ));
+        return;
+      case TypeKey.bool:
+        await _pref.then((p) => p.getBool( key ));
+        return;
+      case TypeKey.double:
+        await _pref.then((p) => p.getDouble( key ));
+        return;
+      default:
+        return;
+    }
+  }
+
+  Future<void> clearCache() async {
+    await _pref.then(( p ) => p.clear());
+  }
+
+  Future<void> removeItemFromCache( String key ) async {
+    await _pref.then((p) => p.remove(key));
+  }
+}
+
+enum TypeKey {
+  bool,
+  string,
+  int,
+  dynamic,
+  double
+  // ... OR MORE...
 }
