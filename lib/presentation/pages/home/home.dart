c79565ffd82c04ce374c8c921960686492cba802
Davidemanoel7 <david.emanoel.706@ufrn.edu.br>
2024-12-14 15:14:54 -0300

    Setup TabBar

--- lib/presentation/pages/home/home.dart
+++ lib/presentation/pages/home/home.dart
@@ -1,26 +1,34 @@
+import 'package:cbc/core/widgets/home_appbar.dart';
+import 'package:cbc/presentation/pages/home/home_tab_screen.dart';
+import 'package:cbc/presentation/pages/pefil/perfil_tab_screen.dart';
+import 'package:cbc/presentation/pages/pesquisa/pesquisa_tab_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../../../providers/auth_provider.dart';
 
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:  const Text('Home'),
-        actions: [
-          IconButton(
-            onPressed: ( ) => Provider.of<AuthProvider>(context, listen: false ).logout(),
-            icon: const Icon( Icons.logout )
-          )
-        ],
-      ),
-      body: const Center(
-        child: Text("Welcome!")
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: const HomeAppBar( sizeHeight: 120 ),
+        body: const TabBarView(
+          children: [
+            HomeTabScreen(),
+            PesquisaTabScreen(),
+            PerfilTabScreen(),
+          ],
+        ),
+        bottomNavigationBar: TabBar(
+          indicatorColor: Theme.of(context).colorScheme.primary,
+          labelColor: Theme.of(context).colorScheme.primary,
+          unselectedLabelColor: Colors.grey.shade400,
+          tabs: const [
+            Tab(icon: Icon(Icons.home), text: 'Home'),
+            Tab(icon: Icon(Icons.shopping_basket_outlined), text: 'Pesquisar'),
+            Tab(icon: Icon(Icons.person), text: 'Perfil'),
+          ],
+        ),
       ),
     );
   }
